69070d3c46227c389dfe844be50e672a3a1fee6f
HasanliGunel <hasanligunel@hotmail.com>
2021-06-23 10:28:36 +0400

    first commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIStudentClass.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student(){ID=1, Name="Lale",Surname="Eliyeva",Age=25},
+                new Student(){ID=2,Name="Tahir",Surname="Meherremov",Age=30}
+            };
+            return Ok(students);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# APIStudentClass
+# APIStudentClass
